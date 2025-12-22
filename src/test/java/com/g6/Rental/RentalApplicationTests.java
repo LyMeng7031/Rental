6a53a92697eff1fb878bebaaad465934ae85d021
Ly meng <hort.lymeng@institute.pse.ngo>
2025-12-22 16:06:58 +0700

    first commit

---
+++ src/test/java/com/g6/Rental/RentalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.g6.Rental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
