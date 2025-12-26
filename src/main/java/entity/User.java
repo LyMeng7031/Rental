fef3d034d2b787eaf160569c9985d8473b1f5828
Tryvan <pich.trivan@institute.pse.ngo>
2025-12-26 07:20:17 +0700

    add entity user

---
+++ src/main/java/entity/User.java
@@ -0,0 +1,49 @@
+package entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long userId;
+
+    @Column(name = "full_name", nullable = false)
+    private String fullName;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column
+    private String phone;
+
+    @Column
+    private String role;
+
+    @Column(name = "profile_image")
+    private String profileImage;
+
+    @Column
+    private String status;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+}
